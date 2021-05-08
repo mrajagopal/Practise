a9ad680ad34c9ec1f44cbebff440a656b5869f30
Madhu RAJAGOPAL <rajagopal.madhu@gmail.com>
2021-05-08 15:34:53 +1200

    Add before and after insert functions

--- LinkedList/CircularLinkedList.cpp
+++ LinkedList/CircularLinkedList.cpp
@@ -17,6 +17,8 @@ public:
   bool append(int v);
   bool traverse();
   bool remove(int v);
+  bool insertBefore(int b, int v);
+  bool insertAfter(int a, int v);
   int size();
   
 private:
@@ -90,6 +92,54 @@ bool CircularLinkedList::traverse()
   return true;
 }
 
+bool CircularLinkedList::insertBefore(int b, int v)
+{
+  Node *n = head;
+  Node *prev = nullptr;
+  while (n != nullptr)// && tail->next != head)
+  {
+    if (n->value == b)
+    {
+      Node *newNode = new Node(v);
+      newNode->next = n;
+      if (prev)
+      {
+        prev->next = newNode;
+      }
+      else // We are inserting before the head
+      {
+        tail->next = newNode;
+        head = newNode;
+      }
+      return true;
+    }
+    prev = n;
+    n = n->next;
+  }
+  return false;
+}
+
+bool CircularLinkedList::insertAfter(int a, int v)
+{
+  Node *n = head;
+  while (n != nullptr)
+  {
+    if (n->value == a)
+    {
+      Node *newNode = new Node(v);
+      newNode->next = n->next;
+      n->next = newNode;
+      if (n == tail)
+      {
+        tail = newNode;
+      }
+      return true;
+    }
+    n = n->next;
+  }
+  return false;
+}
+
 bool CircularLinkedList::remove(int v)
 {
   Node *n = head;
@@ -157,7 +207,6 @@ int main()
   }
 
   c.traverse();
-  std::cout << "size: " << c.size() << std::endl;
   c.remove(5);
   c.traverse();
   c.remove(1);
@@ -167,6 +216,18 @@ int main()
   c.remove(10);
   c.traverse();
   c.remove(3);
+  c.traverse();
+  c.remove(2);
+  c.traverse();
+  c.insertAfter(9,1);
+  c.traverse();
+  c.insertBefore(9,10);
+  c.traverse();
+  c.insertBefore(4,100);
+  c.traverse();
+  c.insertBefore(100,110);
+  c.traverse();
+  c.insertAfter(100,105);
   c.traverse();
 
   return 0;
