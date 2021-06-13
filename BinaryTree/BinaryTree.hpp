b2a460c86bcce594dd9121d6a6666bf1b1f1e98d
Madhu RAJAGOPAL <rajagopal.madhu@gmail.com>
2021-06-14 09:57:09 +1200

    CPP Binary Tree implementation: Insertion only

---
+++ BinaryTree/BinaryTree.hpp
@@ -0,0 +1,39 @@
+//
+//  BinaryTree.hpp
+//  
+//
+//  Created by Madhu RAJAGOPAL on 13/6/21.
+//
+
+#ifndef BinaryTree_hpp
+#define BinaryTree_hpp
+
+#include <iostream>
+
+class BSTNode
+{
+public:
+  BSTNode(int d)
+   : data(d),
+     left(nullptr),
+     right(nullptr)
+  {};
+  ~BSTNode(){};
+  int data;
+  BSTNode *left;
+  BSTNode *right;
+};
+
+class BST
+{
+public:
+  BST();
+  ~BST();
+  BSTNode * insert(BSTNode *node, int d);
+  void remove(BSTNode *node, int d);
+  void erase(BSTNode *node);
+  BSTNode * findMin(BSTNode *node);
+  BSTNode *root;
+};
+
+#endif /* BinaryTree_hpp */
