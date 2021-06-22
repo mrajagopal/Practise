c932aa1aca74ded33994d3e56595a7a398e37824
stoyosawa <s.toyosawa@f5.com>
2021-06-22 12:20:26 +1200

    README updated

--- BinaryTree/BinaryTreeDict.py
+++ BinaryTree/BinaryTreeDict.py
@@ -3,42 +3,84 @@
 
 
 TREE = {
-	'value': 8,
+	'value': 15,
 	'left': {
-		'value': 3,
+		'value': 10,
 		'left': {
-			'value': 1
+			'value': 8
 		},
 		'right': {
-			'value': 6,
-			'left': {
-				'value': 4,
-			},
-			'right': {
-				'value': 7
-			}
+			'value': 12
 		}
 	},
 	'right': {
-		'value': 10,
+		'value': 20,
+		'left': {
+			'value': 16
+		},
 		'right': {
-			'value': 14,
-			'left': {
-				'value': 13
-			}
+			'value': 25
 		}
 	}
 }
 
-def traverse_tree(tree, level=0, accum=[], ):
-    accum.append((level, tree['value']))
-    for left_right in ['left', 'right']:
-        if left_right in tree:
-            accum = traverse_tree(tree[left_right], level+1, accum)
-    return accum
+
+### ----- Traversal methods ------
+
+def traverse_preorder(tree, level=0, accum=[]):
+	'''
+	Depth-first, Pre-order
+	Print the node upon first visit in the traversal.
+	The output retains the structure of the tree (serialization).
+	Example: 15 10 8 12 20 16 25
+	'''
+	accum.append((level, tree['value']))
+	for left_right in ['left', 'right']:
+		if left_right in tree:
+			accum = traverse_preorder(tree[left_right], level+1, accum)
+	return accum
+
+
+def traverse_inorder(tree, level=0, accum=[]):
+	'''
+	Depth-first, In-order
+	Print the node upon the second visit in the traversal.
+	The output is sorderd (ascending).
+	For decending order, visit right first (instead of left-first).
+	Example: 8 10 12 15 16 20 25
+	'''
+	for left_right in ['left', 'right']:
+		if left_right in tree:
+			accum = traverse_inorder(tree[left_right], level+1, accum)
+	accum.append((level, tree['value']))
+	return accum
+
+
+def traverse_postorder(tree, level=0, accum=[]):
+	'''
+	Depth-first, Post-order
+	Print the node when it is last visisted in the traversal.
+	The output shows the order of deletion from leafs to root.
+	Example: 8 12 10 16 25 20 15
+	'''
+	for left_right in ['left', 'right']:
+		if left_right in tree:
+			accum = traverse_inorder(tree[left_right], level+1, accum)
+		else:
+			accum.append((level, tree['value']))
+	return accum
+
+
+
+def traverse_values(accum):
+	'''
+	Obtain the list of values from the list of accumulated node list (from the above traversal methods).
+	'''
+	return [node[1] for node in accum]
 
 
 def print_tree(tree, level=0):
+	''' preorder print '''
 	print('{}{}'.format(level*' ', tree['value']))
 	for left_right in ['left', 'right']:
 		if left_right in tree:
@@ -48,7 +90,7 @@ def print_tree(tree, level=0):
 def get_path_by_value(tree, value, path=[]):
 	'''
 	A search routine. Look for the value in the tree.
-	Return it's path to the node: e.g., ['left', 'left', 'value'].
+	Return the path to the node: e.g., ['left', 'left', 'value'].
 	Return [] if no value is found.
 	'''
 	if tree['value'] == value:                                 # Found you.
@@ -68,7 +110,8 @@ def get_path_by_value(tree, value, path=[]):
 
 def get_value_from_path(obj, path):
 	'''
-	Auxiliary function for getting the value from the tree path (see get_path_by_value)
+	Auxiliary function to get the value from the tree path (see get_path_by_value).
+	Return None if there is no such path (including an empty path)
 	'''
 	if len(path) == 0:                                         # Empty path means no value found
 		return None
@@ -88,8 +131,10 @@ def get_value_from_path(obj, path):
 
 def insert(tree, value):
 	'''
-	Insert a new leaf to the tree
-	'''
+	Insert a new leaf to the tree.
+	If tree is None, there is no place to insert (in this implementation). Return 
+	'tree' should not be empty (e.g., {}). 
+
 	if not tree:                                               # Tree is empty. Create a new root node
 		return {'value': value}
 
@@ -106,12 +151,23 @@ def insert(tree, value):
 		return get_path_by_value(tree['right'], value, path)
 
 	return []            	
+	'''
+	pass
+
+print('--- Testing the traversal methods ---')
+preorder = traverse_preorder(TREE)
+print('1) Pre-order:   ', traverse_values(preorder))
+inorder = traverse_inorder(TREE)
+print('2) In-ordert:   ', traverse_values(inorder))
+postorder = traverse_postorder(TREE)
+print('3) Post-ordert: ', traverse_values(postorder))
 
 
-print('-- Testing print_tree --')
-print_tree(TREE)
 
+
+'''
 print('-- Testing get_path_by_value --')
 for value in [8, 3, 1, 6, 4, 7, 10, 14, 13, 100]:
 	path = get_path_by_value(TREE, value, path=[])	
 	print('{} -> {} -> {}'.format(value, path, get_value_from_path(TREE, path)))
+'''
